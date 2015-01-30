ce8b4fd62982b5badcafb37ee89c76f679d8052b
Mark Baker <mbaker@analecta.com>
2015-01-30 17:24:40 -0500

    Added tests and fixed bugs in URL rewrite object

--- rewriteurl.py
+++ rewriteurl.py
@@ -88,17 +88,42 @@ class URLRewriter:
             for rewrite in cat.xml.iterfind('.//{urn:oasis:names:tc:entity:xmlns:xml:catalog}rewriteURI'):
                 rewrite_list.add(Rewrite(rewrite.attrib['uriStartString'],
                                          rewrite.attrib['rewritePrefix'],
-                                         cat.file_name))
+                                         os.path.dirname(cat.file_name)))
         return rewrite_list
 
-    def rewrite_url(self,url):
+    def rewrite_url(self, url, allowed_prefixes=None):
+        if isinstance(allowed_prefixes, list):
+            prefixes = allowed_prefixes
+        elif isinstance(allowed_prefixes, str):
+            prefixes = [allowed_prefixes]
+        else:
+            prefixes=None
+
         for rewrite in self.rewrite_list:
             if url.startswith(rewrite.start):
                 return os.path.abspath(os.path.join(rewrite.base, rewrite.prefix + url[len(rewrite.start):]))
+
+        if prefixes is not None:
+            for prefix in prefixes:
+                if url.startswith(prefix):
+                    return url
+
         raise KeyError("uriStartString not found.")
 
 if __name__ == '__main__':
-    re=URLRewriter('C:/Users/Mark/spfe-open-toolkit/catalog.xml')
-    print(re.catalogs)
-    print(re.rewrite_list)
-    print(re.rewrite_url('http://spfeopentoolkit.org/spfe-ot/1.0/schemas/config/spfe-config.xsd'))
+    re=URLRewriter(['C:/Users/Mark/spfe-open-toolkit/catalog.xml',r'C:\Users\Mark\spfe-open-toolkit\spfe-ot\1.0\catalog.xml'])
+    print("Test if catalog list created correctly:", end=" ")
+    print('PASS' if type(list(re.catalogs)[1]) is Catalog else 'FAIL')
+    print("Test if rewrite list created correctly:", end=" ")
+    print('PASS' if type(list(re.rewrite_list)[1]) is Rewrite else 'FAIL')
+    print("Test for successful catalog lookup.", end=" ")
+    print('PASS' if os.path.exists(re.rewrite_url('http://spfeopentoolkit.org/spfe-ot/1.0/schemas/config/spfe-config.xsd')) else "FAIL")
+    print("Test for successful prefix override.", end=" ")
+    print('PASS' if re.rewrite_url('c:/Users/Mark/spfe-open-toolkit/spfe-ot/1.0/schemas/config/spfe-config.xsd',
+                     "c:/Users/Mark") == 'c:/Users/Mark/spfe-open-toolkit/spfe-ot/1.0/schemas/config/spfe-config.xsd'
+                 else "FAIL")
+    print("Test for successful KeyError generation if lookup fails.", end=" ")
+    try:
+        print(re.rewrite_url('c:/Users/Mark/spfe-open-toolkit/spfe-ot/1.0/schemas/config/spfe-config.xsd'))
+    except KeyError as e:
+        print("PASS" if str(e) == str("'uriStartString not found.'") else 'FAIL')
