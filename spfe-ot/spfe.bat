58fc437dc86f2bf4d1b1ac78657affdfe651a8ef
Mark Baker <mbaker@analecta.com>
2015-03-02 17:21:31 -0500

    Removing the need for SPFEOT_HOME

--- spfe-ot/spfe.bat
+++ spfe-ot/spfe.bat
@@ -2,6 +2,6 @@
 REM This file is part of the SPFE Open Toolkit. See the accompanying license.txt file for applicable licenses.
 REM (c) Copyright Analecta Communications Inc. 2015 All Rights Reserved.
 
-py -3 %SPFEOT_HOME%/spfe.py %*
+py -3 %~dp0/spfe.py %*
 
 
--- spfe-ot/spfe.sh
+++ spfe-ot/spfe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This file is part of the SPFE Open Toolkit. See the accompanying license.txt file for applicable licenses.
 # (c) Copyright Analecta Communications Inc. 2012 All Rights Reserved.
-python3 $SPFEOT_HOME/spfe.py "$@"
+python3 "$(dirname "$0")/spfe.py" "$@"
 
