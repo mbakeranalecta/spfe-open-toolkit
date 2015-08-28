ee24a5aa077b82544a6cb20729f12001741ee2a1
Mark Baker <mbaker@analecta.com>
2015-08-28 15:08:45 -0400

    Beep when build is done

--- spfe-ot/spfe.py
+++ spfe-ot/spfe.py
@@ -29,7 +29,8 @@ def do_build(build_args):
         config.write_script_files()
         spfelib.build.build_content_set(config)
         end_time = datetime.now()
-        print('Build completed in : {}'.format(end_time - start_time))
+        # \a to sound a beep on completion
+        print('\aBuild completed in : {}'.format(end_time - start_time))
     else:
         print("Config file not found: " + build_args.config_file)
         sys.exit(1)
